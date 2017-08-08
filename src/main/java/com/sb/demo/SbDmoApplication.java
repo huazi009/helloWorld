5538bdbfcfee5707b7b77434f2df80422917b4ec
zhliu <zhliu@1sw6232.99wuxian.com>
2017-08-08 15:28:07 +0800

    ddd

---
+++ src/main/java/com/sb/demo/SbDmoApplication.java
@@ -0,0 +1,12 @@
+package com.sb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDmoApplication.class, args);
+	}
+}
